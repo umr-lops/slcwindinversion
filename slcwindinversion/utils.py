1b02b3eb7111efe49fffb49a7c23cc7bc05ae6e6
agrouaze <antoine.grouazel@gmail.com>
2024-02-27 10:44:18 +0100

    modifications on the method to get output file path

--- slcwindinversion/utils.py
+++ slcwindinversion/utils.py
@@ -13,35 +13,33 @@ def get_l2_filepath(l1c_fullpath, version, outputdir)->str:
         l2_full_path: str
     """
     safe_file = os.path.basename(os.path.dirname(l1c_fullpath))
-    if outputdir is None:
-        run_directory = os.path.dirname(os.path.dirname(l1c_fullpath))
-        # Output file directory
-        pathout_root = run_directory.replace("l1c", "l2")
-    else:
-        pathout_root = outputdir
+    pathout_root = outputdir
 
     safe_start_date = datetime.datetime.strptime(safe_file.split('_')[5],'%Y%m%dT%H%M%S')
-    pathout = os.path.join(pathout_root, safe_start_date.strftime('%Y'),safe_start_date.strftime('%j'), safe_file)
+    pathout = os.path.join(pathout_root, safe_start_date.strftime('%Y'),safe_start_date.strftime('%j'))
 
-    # Output filename
-    l2_full_path = os.path.join(
-        pathout, os.path.basename(l1c_fullpath)).replace("L1C", "L2").replace('XSP','WSP')
-    # add the product ID in the SAFE  name
-    basesafe = os.path.basename(os.path.dirname(l2_full_path))
-    basesafe0 = copy.copy(basesafe)
+    # SAFE part
+    safe_file = safe_file.replace("L1C", "L2").replace('XSP', 'WSP')
     if (
-        len(basesafe.split("_")) == 10
+        len(safe_file.split("_")) == 10
     ):  # classical ESA SLC naming #:TODO once xsarslc will be updated this case could be removed
-        basesafe = basesafe.replace(".SAFE", "_" + version.upper() + ".SAFE")
+        safe_file = safe_file.replace(".SAFE", "_" + version.upper() + ".SAFE")
     else:  # there is already a product ID in the L1B SAFE name
-        lastpart = basesafe.split("_")[-1]
-        basesafe = basesafe.replace(lastpart, version.upper() + ".SAFE")
-    l2_full_path = l2_full_path.replace(basesafe0, basesafe)
+        lastpart = safe_file.split("_")[-1]
+        safe_file = safe_file.replace(lastpart, version.upper() + ".SAFE")
 
-    lastpiece = l2_full_path.split("_")[-1]
+    # measurement part
+    base_measu = os.path.basename(l1c_fullpath)
+    lastpiece = base_measu.split("-")[-1]
 
-    l2_full_path = l2_full_path.replace(lastpiece, version + ".nc")
+    base_measu = base_measu.replace(lastpiece, version.lower() + ".nc")
+    if base_measu[0:3]=='l1c':
+        base_measu = base_measu.replace('l1c-','l2-')
+    else:
+        base_measu = 'l2-'+base_measu
+    base_measu = base_measu.replace('-xsp-','-wsp-')
 
+    l2_full_path = os.path.join(pathout, safe_file,base_measu)
     logging.debug("File out: %s ", l2_full_path)
     if not os.path.exists(os.path.dirname(l2_full_path)):
         os.makedirs(os.path.dirname(l2_full_path), 0o0775)
