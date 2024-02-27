bb710f369cd87c2e9961a721e99a6deaf1a6493b
agrouaze <antoine.grouazel@gmail.com>
2024-02-27 11:41:25 +0100

    integrate the method to get output fiels

--- slcwindinversion/mainslcwindinversion.py
+++ slcwindinversion/mainslcwindinversion.py
@@ -8,7 +8,8 @@ import os
 from xsarsea import windspeed
 from scipy.ndimage import binary_dilation
 import logging
-
+from slcwindinversion.utils import get_l2_filepath
+import slcwindinversion
 
 def get_vv_vh(folder, subswath_number):
     """
@@ -27,43 +28,30 @@ def get_vv_vh(folder, subswath_number):
         return vvs[0], vhs[0]
 
 
-def core_inversion(folder, outd, overwrite=False,version=None):
+def core_inversion(input_folder, outd,version, overwrite=False):
     """
     args:
-        folder: str input L1C or L1B SAFE full path containing .nc files with sigma0,incidence,ground_heading,nesz variables
+        input_folder: str input L1C or L1B SAFE full path containing .nc files with sigma0,incidence,ground_heading,nesz variables
         outd: str: output directory where to store the L2A WSP .nc files
-        overwrite: bool
+        version: str version (ie product ID) of the L2 WSP product to be generated, for instance C03
+        overwrite: bool, True -> existing output file is replaced, False -> existing output file are kept
     """
-    folder = folder.rstrip('/')
-    out_folder = os.path.join(outd, os.path.basename(folder))
-    out_folder = out_folder.replace('XSP', 'WSP')
-    logging.info(out_folder)
-    os.makedirs(out_folder, exist_ok=True)
-    if '1SDV' in folder or '1SDH' in folder:
-        # even for dual pol there is only 3 output files (co and cross pol are merged)
-        test_all_measu = len(glob.glob(os.path.join(out_folder , "*.nc"))) == 3
-    else:
-        test_all_measu = len(glob.glob(os.path.join(out_folder , "*.nc"))) == 3
-    if os.path.exists(folder) and test_all_measu and overwrite is False:
-        logging.info("output folder %s already exists and expected files are present." % out_folder)
-        return out_folder
+    input_folder = input_folder.rstrip('/')
+    out_folder = outd
 
     subswath_numbers = [1, 2, 3]  # for IW
     for subswath_number in subswath_numbers:
         logging.info("subswath_number %d" % subswath_number)
         try:
-            path_vv, path_vh = get_vv_vh(folder, subswath_number)
+            path_vv, path_vh = get_vv_vh(input_folder, subswath_number)
         except Exception as e:
             return None
-        base_final = os.path.basename(path_vv).replace("-vv-", "-dualpol-")
-        found_version = base_final.split('_')[-1].replace('.nc','')
-        if version is None:
-            outputversion = found_version
-            logging.debug('output files will have same version (%s) than input Level-1C measurements',outputversion)
-        else:
-            outputversion = version
-            logging.debug("output files will have version %s (Level-1C files have version %s)",outputversion,found_version)
-        base_final = base_final.replace(found_version,outputversion)
+
+        l2_wsp_full_path = get_l2_filepath(l1c_fullpath=path_vv, version=version, outputdir=outd)
+        out_folder = os.path.dirname(l2_wsp_full_path)
+        if os.path.exists(l2_wsp_full_path) and overwrite is False:
+            logging.info('%s already exists',l2_wsp_full_path)
+            continue
         for burst_type in ["intra", "inter"]:
             logging.info("burst %s" % burst_type)
 
@@ -74,8 +62,11 @@ def core_inversion(folder, outd, overwrite=False,version=None):
             ancillary_wind = (vv_ds.U10 + 1j * vv_ds.V10) * np.exp(1j * np.deg2rad(vv_ds.ground_heading))
             dsig_cr = windspeed.get_dsig("gmf_s1_v2",vv_ds.incidence,vh_ds.sigma0,vh_ds.nesz)
             """
-            vv_ds = xr.open_dataset(path_vv, group=burst_type + 'burst')
-            vh_ds = xr.open_dataset(path_vh, group=burst_type + 'burst')
+            ds_datatree = xr.open_dataset(path_vv)
+            l1c_product_id = ds_datatree.attrs['product_version']
+            ds_datatree.close()
+            vv_ds = xr.open_dataset(path_vv, group=burst_type + 'burst',engine='h5netcdf')
+            vh_ds = xr.open_dataset(path_vh, group=burst_type + 'burst',engine='h5netcdf')
 
             mask_flag = binary_dilation(vv_ds.land_flag.values.astype('uint8'),
                                         structure=np.ones((3, 3, 3), np.uint8), iterations=3)
@@ -113,6 +104,17 @@ def core_inversion(folder, outd, overwrite=False,version=None):
             out_ds = xr.Dataset()
             out_ds.attrs = vv_ds.attrs
             out_ds.attrs['product'] = 'WSP'
+            out_ds.attrs['processor_name'] = 'slcwindinversion'
+            if 'name' in out_ds.attrs:
+                del out_ds.attrs['name']
+                # out_ds.attrs = out_ds.attrs.pop('name')
+            out_ds.attrs['processor_version'] = slcwindinversion.__version__
+            out_ds.attrs['product_ID_L2_WSP'] = version
+            if os.path.basename(path_vv).split('_')[-1].replace('.SAFE','')[0]=='B':
+                out_ds.attrs['product_ID_L1C_XSP'] = os.path.basename(path_vv).split('_')[-1].replace('.SAFE','').upper()
+            else:
+                out_ds.attrs['product_ID_L1C_XSP'] = l1c_product_id
+
             out_ds["wind_speed_co"] = wind_speed_co
             out_ds["wind_speed_dual"] = wind_speed_dual
             out_ds["wind_speed_co"].attrs["comment"] = out_ds["wind_speed_co"].attrs["comment"].replace(
@@ -137,18 +139,11 @@ def core_inversion(folder, outd, overwrite=False,version=None):
 
 
             # SAVE
-            logging.debug('base_final',base_final)
-            out_ds.to_netcdf(os.path.join(out_folder, base_final), group=burst_type + 'burst', mode='a')
+            # logging.debug('base_final',base_final)
+            # out_ds.to_netcdf(os.path.join(out_folder, base_final), group=burst_type + 'burst', mode='a')
             # vh_ds.to_netcdf(os.path.join(out_folder,os.path.basename(path_vh)),group=burst_type+'burst',mode='a')
-
+            out_ds.to_netcdf(l2_wsp_full_path, group=burst_type + 'burst', mode='a')
             out_ds.close()
             # vh_ds.close()
     logging.info('new netCDF are stored in : %s',out_folder)
     return out_folder
-
-
-# def generate_L2A_windspeed_product(input_directory, output_directory):
-#     folders = glob.glob(os.path.join(input_directory, "*SAFE"))
-#     logging.info('nb SAFE to treat: %s',len(folders))
-#     for folder in folders:
-#         core_inversion(folder, output_directory, overwrite=True)
